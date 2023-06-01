92ee99e5585080c9ae760c644ba9805e214cc74b
mtremmel <m.tremmel6@gmail.com>
2023-06-01 16:19:49 +0100

    refine functions, fix bugs, add comments to bh growth tracking code

--- bhtools/tangos_interface/bh_growth.py
+++ bhtools/tangos_interface/bh_growth.py
@@ -3,32 +3,40 @@ import numpy as np
 _default_dt = 0.02
 
 def init_mdot(tend, dt):
+	"""initialize a time array and an mdot array given a final time tend and time resolution dt"""
 	tarray = np.arange(0,tend+dt, dt)
-	return tarray, np.zeros(tarray)
+	return tarray, np.zeros(len(tarray))
 
 def combine_mdot(raw_mdot_list, time, dt):
+	"""combine a list of raw mdot_histogram outputs from tangos into a single array,
+	working backwards in time such that overlapping timesteps are overwritten by
+	earlier branches of the merger tree"""
 	tarray, mdot_array = init_mdot(time.max(), dt)
 	if len(raw_mdot_list) != len(time):
 		raise RuntimeError("Time and Mdot data not the same length!")
 	for i in range(len(raw_mdot_list)):
 		tmin = time[i] - len(raw_mdot_list[i])*dt
 		tmax = time[i]
-		ind = np.where((tarray>=tmin & tarray <=tmax))[0]
+		ind = np.where((tarray>=tmin) & (tarray <=tmax))[0]
 		mdot_array[ind] = raw_mdot_list[i]
 
 	return tarray, mdot_array
 
-def collect_mdot(halo, bh_link):
-	mdot_hist_list, time_list = halo.calculate_for_progenitors(bh_link+'.raw(BH_mdot_histogram)','t()')
-	return mdot_hist_list, time_list
-
 def trace_bh_galaxy(halo, bh_link_string):
-	mdot_hist_list, time_list = collect_mdot(halo, bh_link_string)
+	"""collect and combine raw BH histograms for selected BHs along merger tree"""
+	mdot_hist_list, time_list = halo.calculate_for_progenitors(bh_link_string + '.raw(BH_mdot_histogram)', 't()')
 	dt = halo.timestep.simulation.get('histogram_delta_t_Gyr', _default_dt)
 	tarray, mdot_array = combine_mdot(mdot_hist_list, time_list, dt)
 	return mdot_array, tarray
 
 def gal_bh_acc_hist(halo, *constraints, selection='BH_mass', minmax='max'):
+	"""Traces along a galaxy's main progenitor branch and connects indivitual BH accretion rate
+	histograms along that tree, selected by BH property constraints at each timestep.
+
+	For example, one might want to exctract the accretion rate for the most massive central (D<1 kpc)
+	BH within a galaxy. If that BH went through a merger event, its individual accretion history might not
+	be representative of the accretion history of the BH within the target galaxy because iords are
+	arbitrarily kept/removed following BH merger events."""
 	link_string = 'link(BH_central,'
 	link_string += selection +','+'"'+minmax+'"'
 	for i in range(len(constraints)):
