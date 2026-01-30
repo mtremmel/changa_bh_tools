fix tform units in orbit object

bhtools/output_reader/orbit.py
@@ -179,7 +179,7 @@ class BHOrbitData(object):
 			sliords[(sliords<0)] = 2*2147483648 + sliords[(sliords<0)]
 			ord = np.argsort(sliords)
 			bhind, = np.where(np.in1d(sliords[ord], self.bhiords))
-			self.tform = sl['tform'][ord][bhind] * -1
+			self.tform = sl['tform'].in_units('Gyr')[ord][bhind] * -1
 			if self.tform.min() < 0: print("WARNING! Positive tforms were found for BHs!")
 			gc.collect()
 		else: